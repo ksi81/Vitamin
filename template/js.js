9c95eab9b725c74bd962dcbca0b627ee3de9cc77
Serhii Klymenko <klimenko81@gmail.com>
2019-01-23 12:11:57 +0200

    Create git repo
    new progect for MK

---
+++ template/js.js
@@ -0,0 +1,78 @@
+// $(function() {
+//     // Multiple images preview in browser
+//     var imagesPreview = function(input, placeToInsertImagePreview) {
+
+//         if (input.files) {
+//             var filesAmount = input.files.length;
+
+//             for (i = 0; i < filesAmount; i++) {
+//                 var reader = new FileReader();
+
+//                 reader.onload = function(event) {
+//                     $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+//                 }
+
+// 				reader.readAsDataURL(input.files[i]);
+
+//             }
+//         }
+
+//     };
+
+//     $('#gallery-photo-add').on('change', function() {
+//         imagesPreview(this, 'div.gallery');
+//     });
+// });
+
+
+
+function handleFileSelectSingle(evt) {
+	var file = evt.target.files; // FileList object
+	var f = file[0]
+	// Only process image files.
+	if (!f.type.match('image.*')) {
+		alert("Только изображения....");
+	}
+	var reader = new FileReader();
+
+	// Closure to capture the file information.
+	reader.onload = (function (theFile) {
+		return function (e) {
+			// Render thumbnail.
+			var span = document.createElement('div');
+			span.innerHTML = ['<div class="mycontainer"><img  id="myImg" class="img-thumbnail image" src="', e.target.result, '" title="', escape(theFile.name), '"/><div class="middle"><div class="text">John Doe</div><div class="text">ВИДАЛИТИ</div><div class="text">РЕДАГУВАТИ</div>  </div></div>'].join('');
+			var parentSpan = document.getElementById('output');
+			parentSpan.insertBefore(span, parentSpan.firstChild);
+		};
+	})(f);
+
+	// Read in the image file as a data URL.
+	reader.readAsDataURL(f);
+}
+
+
+document.getElementById('file').addEventListener('change', handleFileSelectSingle, false);
+
+function handleFileSelectMulti(evt) {
+	var files = evt.target.files; // FileList object
+	var parentSpan = document.getElementById('outputMulti');
+	for (var i = 0, f; f = files[i]; i++) {
+		// Only process image files.
+		if (!f.type.match('image.*')) {
+			alert("Только изображения....");
+		}
+		var reader = new FileReader();
+		// Closure to capture the file information.
+		reader.onload = (function (theFile) {
+			return function (e) {
+				// Render thumbnail.
+				var span = document.createElement('div');
+				span.innerHTML = ['<div class="mycontainer"><img  id="myImg" class="img-thumbnail image" data-toggle="modal" data-target="#myModal" src="', e.target.result, '" title="', escape(theFile.name), '"/></div>'].join('');
+				document.getElementById('outputMulti').insertBefore(span, parentSpan.firstChild);
+			};
+		})(f);
+		// Read in the image file as a data URL.
+		reader.readAsDataURL(f);
+	}
+}
+document.getElementById('fileMulti').addEventListener('change', handleFileSelectMulti, false);
